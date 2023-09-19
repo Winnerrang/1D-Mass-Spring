1c7d8e40808f54046ead9ebbb8c8472e297b0496
Winnerrang <leo.hanxu@mail.utoronto.ca>
2023-09-19 18:56:45 -0400

    Finish runge_kutta damping problem

--- include/runge_kutta.h
+++ include/runge_kutta.h
@@ -16,28 +16,51 @@ inline void runge_kutta(Eigen::VectorXd &q, Eigen::VectorXd &qdot, double dt, do
 
 	Eigen::VectorXd f1, f2, f3, f4;
 	Eigen::VectorXd q1, q2, q3, q4, qdot1, qdot2, qdot3, qdot4;
+	Eigen::VectorXd vkappa1, vkappa2, vkappa3, vkappa4, qkappa1, qkappa2, qkappa3, qkappa4;
+	double temp_dt;
 	q1 = q;
 	qdot1 = qdot;
 
-	q2 = q1;
-	qdot2 = qdot1;
-	forward_euler(q2, qdot2, dt / 2, mass, force);
-
-	q3 = q2;
-	qdot3 = qdot2;
-	forward_euler(q3, qdot3, dt / 2, mass, force);
-
-	q4 = q3;
-	qdot4 = qdot3;
-	forward_euler(q4, qdot4, dt, mass, force);
-
+	Eigen::VectorXd f;
+	// calcuate kappa1
+	force(f, q, qdot);
+	vkappa1 = (1 / mass) * f;
+	qkappa1 = qdot;
 	
-	force(f1, q1, qdot1);
-	force(f2, q2, qdot2);
-	force(f3, q3, qdot3);
-	force(f4, q4, qdot4);
 
-	qdot += dt / 6.0 * (f1 + 2.0 * f2 + 2.0 * f3 + f4)/mass;
-	q += dt / 6.0 * (qdot1 + 2.0 * qdot2 + 2.0 * qdot3 + qdot4);
+	//calculate y^{t+1/2}
+	temp_dt = dt / 2;
+	qdot2 = qdot + temp_dt * vkappa1;
+	q2 = q + temp_dt * qkappa1;
+
+	//calculate kappa2
+	force(f, q2, qdot2);
+	vkappa2 = (1 / mass) * f;
+	qkappa2 = qdot2;
+
+	//calculate y^{t+1}
+	temp_dt = dt / 2;
+	qdot3 = qdot + temp_dt * vkappa2;
+	q3 = q + temp_dt * qkappa2;
+
+	//calculate kappa3
+	force(f, q3, qdot3);
+	vkappa3 = (1 / mass) * f;
+	qkappa3 = qdot3;
+
+	//calculate y^{t+1} (fake one)
+	temp_dt = dt;
+	qdot4 = qdot + temp_dt * vkappa3;
+	q4 = q + temp_dt * qkappa3;
+
+	//calculate kappa4
+	force(f, q4, qdot4);
+	vkappa4 = (1 / mass) * f;
+	qkappa4 = qdot4;
+
+	// calculate real y^{t+1}
+	qdot = qdot + dt * (vkappa1 + 2 * vkappa2 + 2 * vkappa3 + vkappa4) / 6;
+	q = q + dt * (qkappa1 + 2 * qkappa2 + 2 * qkappa3 + qkappa4) / 6;
+
 
 }
